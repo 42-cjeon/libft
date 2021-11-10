372c8d4afc1ca941578926d0158e95b15bb0ecca
전창민 <jcm2232@gmail.com>
2021-11-10 19:13:31 +0900

    Fix norm

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,20 +6,20 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 21:10:16 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 13:39:17 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 19:13:18 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+	size_t	i;
 
 	if (n == 0)
 		return (0);
 	i = 0;
-	while(*s1 != '\0' && *s1 == *s2 && i + 1 < n)
+	while (*s1 != '\0' && *s1 == *s2 && i + 1 < n)
 	{
 		s1++;
 		s2++;
