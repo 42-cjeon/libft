6ff6535de38541bc8c5614e7c3443eb2a13445db
전창민 <jcm2232@gmail.com>
2021-11-10 18:49:46 +0900

    Fix norm

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,14 +6,14 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 16:08:17 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 16:16:30 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 18:48:17 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	del(lst->content);
 	free(lst);
