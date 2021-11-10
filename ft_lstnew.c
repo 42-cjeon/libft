261cec96ac148d2d377578b75a64c59d22034cf4
전창민 <jcm2232@gmail.com>
2021-11-10 18:53:19 +0900

    Fix norm

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,16 +6,16 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:42:13 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 15:47:56 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 18:52:47 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-	t_list *node;
+	t_list	*node;
 
 	node = (t_list *)malloc(sizeof(t_list));
 	if (node == NULL)
