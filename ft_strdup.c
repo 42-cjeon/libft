539a16113b39704ebe9a1e1d7b4460de899c7429
전창민 <jcm2232@gmail.com>
2021-11-10 19:06:07 +0900

    Fix norm

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,17 +6,17 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 12:51:20 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/09 21:16:59 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 19:05:56 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char *ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-	size_t len;
-	char *str;
+	size_t	len;
+	char	*str;
 
 	len = ft_strlen(s1);
 	str = (char *)malloc(sizeof(char) * (len + 1));
