49b2c8295947e193d2ad553a65e192da550c2f1a
전창민 <jcm2232@gmail.com>
2021-11-10 19:08:23 +0900

    Fix norm

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,18 +6,18 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 14:28:15 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 13:53:42 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 19:08:07 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char *str;
-	size_t prefix_len;
-	size_t suffix_len;
+	char	*str;
+	size_t	prefix_len;
+	size_t	suffix_len;
 
 	prefix_len = ft_strlen(s1);
 	suffix_len = ft_strlen(s2);
