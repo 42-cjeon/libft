10e1714d09789669a1f9a382a6eb01549f573c1e
전창민 <jcm2232@gmail.com>
2021-11-10 18:50:33 +0900

    Fix norm

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,15 +6,15 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 16:38:03 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 16:42:49 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 18:50:01 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstiter(t_list *lst, void (*f)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	while(lst)
+	while (lst)
 	{
 		f(lst->content);
 		lst = lst->next;
