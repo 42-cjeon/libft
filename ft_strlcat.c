775853c2bf92a7900a517ad2cd1fdbd505069bee
전창민 <jcm2232@gmail.com>
2021-11-10 19:10:51 +0900

    Fix norm

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,18 +6,18 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 21:12:13 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 09:33:52 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 19:10:11 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t dst_length;
+	size_t	dst_length;
 
 	dst_length = 0;
-	while(*dst != '\0' && dst_length < dstsize)
+	while (*dst != '\0' && dst_length < dstsize)
 	{
 		dst++;
 		dst_length++;
