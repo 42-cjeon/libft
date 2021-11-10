9aced3bea5c4d95627541d753e43ed3efc902705
전창민 <jcm2232@gmail.com>
2021-11-10 19:14:08 +0900

    Fix norm

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,18 +6,18 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 21:10:35 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 09:37:34 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 19:13:50 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strnstr(const char *heystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *heystack, const char *needle, size_t len)
 {
-	size_t needle_len;
-	size_t heystack_len;
-	size_t i;
-	size_t j;
+	size_t	needle_len;
+	size_t	heystack_len;
+	size_t	i;
+	size_t	j;
 
 	needle_len = ft_strlen(needle);
 	heystack_len = ft_strlen(heystack);
@@ -29,7 +29,7 @@ char *ft_strnstr(const char *heystack, const char *needle, size_t len)
 	while (i <= heystack_len - needle_len)
 	{
 		j = 0;
-		while(j < needle_len && needle[j] == heystack[i + j])
+		while (j < needle_len && needle[j] == heystack[i + j])
 			j++;
 		if (j == needle_len)
 			return ((char *)heystack + i);
