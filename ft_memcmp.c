d075d3440d8b2a215d025c53794b3b342fc49a6e
전창민 <jcm2232@gmail.com>
2021-11-10 18:55:22 +0900

    Fix norm

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,17 +6,17 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 21:06:56 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/09 23:42:50 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 18:55:09 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i;
-	const unsigned char *str1;
-	const unsigned char *str2;
+	size_t				i;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
 
 	str1 = (const unsigned char *)s1;
 	str2 = (const unsigned char *)s2;
