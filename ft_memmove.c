bd83840bcc31d78904656b1fcce57fb1e7be1af1
전창민 <jcm2232@gmail.com>
2021-11-10 18:57:37 +0900

    Fix norm

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,22 +6,20 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 21:06:40 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 15:33:15 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 18:57:09 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t i;
-	char *_dst;
-	const char *_src;
+	size_t		i;
+	char		*_dst;
+	const char	*_src;
 
 	if ((size_t)dst < (size_t)src)
-	{
 		ft_memcpy(dst, src, len);
-	}
 	else if ((size_t)dst > (size_t)src)
 	{
 		i = 0;
@@ -34,10 +32,6 @@ void *ft_memmove(void *dst, const void *src, size_t len)
 			_dst--;
 			i++;
 		}
-	}
-	else
-	{
-		return (dst);
 	}
 	return (dst);
 }
