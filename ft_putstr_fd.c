dd170eb9702b2b0d60ac4139be91699ca203fa82
전창민 <jcm2232@gmail.com>
2021-11-10 19:04:10 +0900

    Fix norm

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,16 +6,16 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 19:14:15 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/09 19:18:13 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 19:03:57 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
-void ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-	size_t len;
+	size_t	len;
 
 	len = ft_strlen(s);
 	write(fd, s, len);
