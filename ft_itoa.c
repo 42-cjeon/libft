00a2aa8922cd342d0c4cc91aa4e1120a014435b5
전창민 <jcm2232@gmail.com>
2021-11-10 18:39:35 +0900

    Fix Norm

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:43:04 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 17:44:14 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 18:39:08 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,55 +15,60 @@
 
 static void	copy_reversed(char *dst, const char *src, int n)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(i < n)
+	while (i < n)
 	{
 		dst[n - i - 1] = src[i];
 		i++;
 	}
 }
 
-static int convert_reversed(int *temp, int index, int k)
+static int	convert_reversed(char *temp, int k, int is_neg)
 {
-	
-}
+	int	len;
 
-char *ft_itoa(int n)
-{
-	char temp[11];
-	char *result;
-	int is_negtive;
-	int i;
-	long long k;
-
-	k = n;
-	i = 0;
-	is_negtive = 0;
-	if (k < 0)
+	len = 0;
+	if (is_neg)
 	{
-		is_negtive = 1;
-		k = -k;
-	}
-	temp[i++] = k % 10 + '0';
-	k /= 10;
-	while (k)
-	{
-		temp[i++] = k % 10 + '0';
-		k /= 10;
-	}
-	i += is_negtive;
-	result = (char *)malloc(sizeof(char) * (i + 1));
-	if (result == NULL)
-		return (NULL);
-	result[i] = '\0';
-	if (is_negtive)
-	{
-		result[0] = '-';
-		copy_reversed(result + 1, temp, i - 1);
+		temp[len++] = '0' - (k % 10);
+		k = -(k / 10);
 	}
 	else
-		copy_reversed(result, temp, i);
+	{
+		temp[len++] = k % 10 + '0';
+		k /= 10;
+	}
+	while (k)
+	{
+		temp[len++] = k % 10 + '0';
+		k /= 10;
+	}
+	return (len + is_neg);
+}
+
+char	*ft_itoa(int n)
+{
+	char	temp[10];
+	char	*result;
+	int		is_neg;
+	int		len;
+
+	is_neg = 0;
+	if (n < 0)
+		is_neg = 1;
+	len = convert_reversed(temp, n, is_neg);
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+	result[len] = '\0';
+	if (is_neg)
+	{
+		result[0] = '-';
+		copy_reversed(result + 1, temp, len - 1);
+	}
+	else
+		copy_reversed(result, temp, len);
 	return (result);
 }
