465afd2f81f14e83e237bcf1347127875870a883
전창민 <jcm2232@gmail.com>
2021-11-10 19:12:25 +0900

    Fix norm

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,18 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 21:12:49 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/09 23:46:49 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 19:12:09 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-	size_t length;
+	size_t	length;
 
 	length = 0;
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		s++;
 		length++;
