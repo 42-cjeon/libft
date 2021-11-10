58418b90fe80a9f8f3bae79dd83f766227cd3853
전창민 <jcm2232@gmail.com>
2021-11-10 18:58:47 +0900

    Fix norm

--- ft_memset.c
+++ ft_memset.c
@@ -1,19 +1,30 @@
-#include "libc.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 21:06:31 by cjeon             #+#    #+#             */
+/*   Updated: 2021/11/10 18:58:11 by cjeon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void *ft_memset(void *b, int c, size_t len)
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char byte;
-	char *memory;
-	size_t i;
+	unsigned char	byte;
+	char			*memory;
+	size_t			i;
 
 	byte = (unsigned char)c;
 	memory = (char *)b;
 	i = 0;
-	while(i < len)
+	while (i < len)
 	{
 		memory[i] = byte;
 		i++;
 	}
-
 	return (b);
 }
