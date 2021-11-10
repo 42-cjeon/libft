5369a57992602b3e308fd40d9cb46d0e4268a662
전창민 <jcm2232@gmail.com>
2021-11-10 18:54:34 +0900

    Fix norm

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,17 +6,17 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 21:07:10 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 00:14:58 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 18:54:15 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char *str;
-	unsigned char find_char;
-	size_t i;
+	const unsigned char	*str;
+	unsigned char		find_char;
+	size_t				i;
 
 	str = (const unsigned char *)s;
 	find_char = (unsigned char)c;
