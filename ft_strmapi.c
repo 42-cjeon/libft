53afcba58d3f8b33c9f09be08626035c2100c1a7
전창민 <jcm2232@gmail.com>
2021-11-10 19:13:03 +0900

    Fix norm

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,25 +6,25 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 18:41:58 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 10:02:07 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/10 19:12:43 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	unsigned int len;
-	unsigned int i;
-	char *str;
+	unsigned int	len;
+	unsigned int	i;
+	char			*str;
 
 	len = ft_strlen(s);
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
 		return (NULL);
 	i = 0;
-	while(i < len)
+	while (i < len)
 	{
 		str[i] = f(i, s[i]);
 		i++;
