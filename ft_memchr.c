1c0843dcd8a1f7671760e62f44c7e05c229bb056
전창민 <jcm2232@gmail.com>
2021-11-11 14:39:21 +0900

    Optimize

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 21:07:10 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 18:54:15 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/11 14:38:46 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,14 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	const unsigned char	*str;
 	unsigned char		find_char;
-	size_t				i;
 
-	str = (const unsigned char *)s;
+	str = s;
 	find_char = (unsigned char)c;
-	i = 0;
-	while (i < n)
+	while (n--)
 	{
 		if (*str == find_char)
-		{
 			return ((void *)str);
-		}
 		str++;
-		i++;
 	}
 	return (NULL);
 }
