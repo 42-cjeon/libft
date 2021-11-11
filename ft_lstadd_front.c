186aa24816e3b62e1a379c572dd80f4580d962c8
전창민 <jcm2232@gmail.com>
2021-11-11 14:09:13 +0900

    Remove null check

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,17 +6,14 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:48:53 by cjeon             #+#    #+#             */
-/*   Updated: 2021/11/10 18:45:33 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/11/11 14:08:06 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (new == NULL)
-		return ;
 	if (lst == NULL)
 		*lst = new;
 	else
